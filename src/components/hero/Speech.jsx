44910bfc790df42be40e16f14c9c7bfc9312ba3a
SiyalSb <maharilyas699@gmail.com>
2025-01-10 10:33:13 +0500

    complete app

---
+++ src/components/hero/Speech.jsx
@@ -0,0 +1,28 @@
+import { TypeAnimation } from "react-type-animation"
+
+
+const Speech = () => {
+  return (
+    <div className="bubbleContainer">
+      <div className="bubble">
+      <TypeAnimation
+      sequence={[
+        1000,
+            "Same substring at the start will only be typed out once, initially",
+            1000,
+            "Lorem ipsum dolor sit amet lerinat consectetur adipisicing. ",
+            1000,
+        1000
+      ]}
+      wrapper="span"
+      speed={40}
+      deletionSpeed={60}
+      repeat={Infinity}
+    />
+      </div>
+      <img src="/man.png" alt="" />
+    </div>
+  )
+}
+
+export default Speech
